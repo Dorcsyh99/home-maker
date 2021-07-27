5a68219516408e68f6a52b3e40c8b90adf5074e8
Dorcsyh99 <dorcsyh99@gmail.com>
2021-07-27 21:10:22 +0200

    home page form improved

--- ClientApp/src/app/home-page/home-page.component.ts
+++ ClientApp/src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  searchForm =  new FormGroup({
+    city: new FormControl(''),
+    minPrice: new FormControl(''),
+    maxPrice: new FormControl(''),
+    type: new FormControl(''),
+  });
   constructor() { }
 
   ngOnInit(): void {
+
   }
+
+
+  onSubmit(form: FormGroup){}
 
 }
